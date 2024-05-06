8c36dedf5df7243c8208665c70a178668064e8b6
Stuff-Stuffs <stevenziehlke@gmail.com>
2024-05-06 10:47:33 -0500

    No death in fake world!

--- src/main/java/io/github/stuff_stuffs/fake_world/common/impl/FakeWorld.java
+++ src/main/java/io/github/stuff_stuffs/fake_world/common/impl/FakeWorld.java
@@ -3,6 +3,8 @@ package io.github.stuff_stuffs.fake_world.common.impl;
 import io.github.stuff_stuffs.fake_world.common.api.FakeWorldApi;
 import io.github.stuff_stuffs.fake_world.common.impl.net.FakeWorldCommonNet;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.entity.event.v1.ServerEntityWorldChangeEvents;
+import net.fabricmc.fabric.api.entity.event.v1.ServerLivingEntityEvents;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.util.Identifier;
@@ -13,11 +15,19 @@ public class FakeWorld implements ModInitializer {
     @Override
     public void onInitialize() {
         FakeWorldCommonNet.init();
+        FakeWorldApi.LEAVE_EVENT.invoker();
         ServerLifecycleEvents.SERVER_STOPPING.register(server -> {
             for (final ServerPlayerEntity player : server.getPlayerManager().getPlayerList()) {
                 FakeWorldApi.leaveFakeWorld(player);
             }
         });
+        ServerLivingEntityEvents.ALLOW_DEATH.register((entity, damageSource, damageAmount) -> {
+            if (entity instanceof final ServerPlayerEntity player) {
+                return ((ServerPlayerExtensions) player).fake_world$data() == null;
+            }
+            return true;
+        });
+        ServerEntityWorldChangeEvents.AFTER_PLAYER_CHANGE_WORLD.register((player, origin, destination) -> FakeWorldApi.leaveFakeWorld(player));
     }
 
     public static Identifier id(final String path) {
